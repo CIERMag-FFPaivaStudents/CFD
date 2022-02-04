8aa74bfb56f98d788d534caf069f159b7e990793
Gustavo Solcia <gustavo.solcia@usp.br>
2022-02-04 08:03:01 -0300

    created mask function

--- ImageProcessing/tofBrainExtraction.py
+++ ImageProcessing/tofBrainExtraction.py
@@ -14,6 +14,58 @@ from fsl.wrappers.misc import fslreorient2std
 from fsl.transform.affine import transform, concat
 from fsl.utils.image.resample import resampleToReference
 
+def transformTof2MaskCoord(tofVoxCoord, tofVox2World, maskWorld2Vox):
+    
+    floatCoord = transform(transform(tofVoxCoord,tofVox2World),
+                            maskWorld2Vox).round()
+
+    transfMaskVoxCoord = [int(item) for item in floatCoord]
+
+    return transfMaskVoxCoord
+
+def getTofMaskAffines(tof, mask):
+
+    tofVox2World = tof.getAffine('voxel', 'world')
+    maskWorld2Vox = mask.getAffine('world','voxel')
+
+    return tofVox2World, maskWorld2Vox
+
+def createBlankImage(baseImage):
+
+    data = np.zeros(baseImage.shape)
+
+    blankImage = Image(data, header=baseImage.header)
+
+    return blankImage
+
+def isInsideMaskSpace(transfMaskVoxCoord, maskShape):
+    boolean = (transfMaskVoxCoord[0]>=0 and transfMaskVoxCoord[0]<maskShape[0]) and \
+                        (transfMaskVoxCoord[1]>=0 and transfMaskVoxCoord[1]<maskShape[1]) and\
+                        (transfMaskVoxCoord[2]>=0 and transfMaskVoxCoord[2]<maskShape[2])
+    return boolean
+
+def convertMask2Tof(tof, mask):
+
+    tofShape = tof.shape
+    maskShape = mask.shape
+
+    tofMask = createBlankImage(tof)
+
+    tofVox2World, maskWorld2Vox = getTofMaskAffines(tof, mask)
+    
+    for i in range(tofShape[0]): #This can take a LONG time
+        for j in range(tofShape[1]): #Clearly three for's is not the best solution
+            for k in range(tofShape[2]): #feel free to contribute with a better solution!
+                
+                tofVoxCoord = [i, j, k]
+                transfMaskVoxCoord = transformTof2MaskCoord(tofVoxCoord, tofVox2World, maskWorld2Vox)
+
+                if(isInsideMaskSpace(transfMaskVoxCoord, maskShape)):
+                    tofMask.data[i,j,k] = mask.data[transfMaskVoxCoord[0],transfMaskVoxCoord[1],
+                                                    transfMaskVoxCoord[2]]
+
+    return tofMask
+
 if __name__=='__main__':
 
     inputT1 = 'IXI362-HH-2051-T1'
@@ -28,19 +80,8 @@ if __name__=='__main__':
 
     mask = Image(inputT1+'_brain_mask')
     tof = Image(inputTOF+'_reorient')
-
-    tofVox2World = tof.getAffine('voxel', 'world')
-    maskWorld2Vox = mask.getAffine('world','voxel')
-
-    print(mask.sameSpace(tof))
-    print(maskVox2World)
-    print(tofWorld2Vox)
     
-    mask2tof = concat(maskVox2World,tofWorld2Vox)
-    tof2mask = concat(tofVox2World,maskWorld2Vox)
+    tofMask = convertMask2Tof(tof, mask)
 
-    print(transform(transform([255,255,0],tofVox2World),maskWorld2Vox).round())
+    tofMask.save(inputTOF+"_brain_mask")
 
-    
-    #I'm still working on this transformation
-    fslroi(inputT1+'_brain_mask', inputT1+'_brain_mask_roi',
